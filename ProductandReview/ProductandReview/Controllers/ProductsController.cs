620647e2338c3ac4d290849b5c5f9c4a0c277768
Kyle Washington <kmw1993@msn.com>
2023-08-24 00:47:29 -0400

    Uploading my completed controllers

---
+++ ProductandReview/ProductandReview/Controllers/ProductsController.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProductandReviewAPI.Data;
+using ProductandReviewAPI.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ProductandReview.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+            
+        // GET: api/<Products>
+        [HttpGet]
+        public IActionResult GetAllProducts()
+        {
+            var products = _context.Products.ToList();
+            return Ok(products);
+        }
+
+        // GET api/<ProductsC>/5
+        [HttpGet("{id}")]
+        public IActionResult GetProduct(int id)
+        {
+            var product = _context.Products
+                .Where(p => p.Id == id)
+                .Include(p => p.Reviews)
+                .SingleOrDefault();
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            double? averageRating = product.Reviews?.Average(r => r.Rating);
+
+            var productDTO = new ProductDTO
+            {
+                Id = product.Id,
+                Name = product.Name,
+                Price = product.Price,
+                Reviews = product.Reviews?.Select(r => new ReviewDTO
+                {
+                    Id = r.Id,
+                    Text = r.Text,
+                    Rating = r.Rating
+                }).ToList(),
+                AverageRating = averageRating.HasValue ? averageRating.Value : 0
+            };
+            return Ok(productDTO);
+        }
+        //GET: api/<Products>
+        [HttpGet]
+        public IActionResult GetAllProducts([FromQuery] double? maxPrice = null)
+        {
+            IQueryable<Product> query = _context.Products;
+            if (maxPrice.HasValue)
+            {
+                query = query.Where(p => p.Price < maxPrice);
+            }
+
+            var products = query.ToList();
+
+            return Ok(products);
+        }
+
+        // POST api/<ProductsController>
+        [HttpPost]
+        public IActionResult AddProduct([FromBody] Product newProduct)
+        {
+            if (newProduct == null) return BadRequest();
+            _context.Products.Add(newProduct);
+            _context.SaveChanges();
+
+            return CreatedAtAction(nameof(GetProduct), new { id = newProduct.Id }, newProduct);
+        }
+
+        // PUT api/<ProductsController>/5
+        [HttpPut("{id}")]
+        public IActionResult UpdateProduct(int id, [FromBody] Product updatedProduct)
+        {
+            var existingProduct = _context.Products.Find(id);
+            if (existingProduct == null)
+                return NotFound();
+            existingProduct.Name = updatedProduct.Name;
+            existingProduct.Price = updatedProduct.Price;
+            existingProduct.Reviews = updatedProduct.Reviews;
+            _context.SaveChanges();
+            return Ok(existingProduct);
+        }
+
+        // DELETE api/<ProductsController>/5
+        [HttpDelete("{id}")]
+        public IActionResult DeleteProduct(int id)
+        {
+            var productToDelete = _context.Products.Find(id);
+            if (productToDelete == null)
+                return NotFound();
+
+            _context.Products.Remove(productToDelete);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+    }
+}
---
+++ ProductandReview/ProductandReview/Controllers/ReviewsController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductandReviewAPI.Data;
+using ProductandReviewAPI.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ProductandReview.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReviewsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ReviewsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        // GET: api/<Reviews>
+        [HttpGet]
+        public IActionResult GetAllReviews()
+        {
+           var reviews = _context.Reviews.ToList();
+            return Ok(reviews);
+        }
+
+        // GET api/<Reviews>/5
+        [HttpGet("{id}")]
+        public IActionResult GetReview(int id)
+        {
+            var review = _context.Reviews.Find(id);
+            if (review == null)
+                return NotFound();
+
+            return Ok(review);
+        }
+
+        //GET api/<Reviews>/ByProductId/{productId}
+        [HttpGet("ByProductId/{productId}")]
+        public IActionResult GetByProductId(int ProductId) 
+        {
+            var reviews = _context.Reviews.Where(r => r.ProductId == ProductId).ToList();
+            return Ok(reviews);
+        }
+
+
+
+        // POST api/<Reviews>
+        [HttpPost]
+        public IActionResult AddReview([FromBody] Review newReview)
+        {
+            if (newReview == null) return BadRequest();
+            _context.Reviews.Add(newReview);
+            _context.SaveChanges();
+
+            return CreatedAtAction(nameof(GetReview), new {id = newReview.Id}, newReview);
+        }
+
+        // PUT api/<Reviews>/5
+        [HttpPut("{id}")]
+        public IActionResult UpdateReview(int id, [FromBody] Review updatedReview)
+        {
+            var existingReview = _context.Reviews.Find(id);
+            if (existingReview == null)
+                return NotFound();
+            
+            existingReview.Text = updatedReview.Text;
+            existingReview.Rating = updatedReview.Rating;
+            existingReview.ProductId = updatedReview.ProductId;
+            _context.SaveChanges();
+            return Ok(existingReview);
+        }
+
+        // DELETE api/<Reviews>/5
+        [HttpDelete("{id}")]
+        public IActionResult DeleteReview(int id)
+        {
+            var reviewToDelete = _context.Reviews.Find(id);
+            if (reviewToDelete == null) 
+                return NotFound();
+
+            _context.Reviews.Remove(reviewToDelete);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+    }
+}
